c3f1dde77b40f235cadbe0416049a93d43960d30
0xDomCobb <0xdomcobb@proton.me>
2024-04-17 20:41:49 +0800

    feat:  asyncio & component

---
+++ xvm/extensibility/__init__.py
@@ -0,0 +1,16 @@
+from xvm.extensibility.asyncio import (  # noqa: F401
+    AsyncioIsolatedComponent,
+)
+from xvm.extensibility.component import (  # noqa: F401
+    Application,
+    BaseComponentAPI,
+    BaseIsolatedComponent,
+    ComponentAPI,
+)
+from xvm.extensibility.component_manager import (  # noqa: F401
+    ComponentManager,
+)
+from xvm.extensibility.trio import (  # noqa: F401
+    TrioComponent,
+    TrioIsolatedComponent,
+)
