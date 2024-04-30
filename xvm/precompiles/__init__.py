d4b1b71a4b6375accee52a67e458f68daf075fcd
0xDomCobb <0xdomcobb@proton.me>
2024-04-30 21:14:12 +0800

    update(precompiles): modexp

--- xvm/precompiles/__init__.py
+++ xvm/precompiles/__init__.py
@@ -2,3 +2,4 @@ from .sha256 import sha256  # noqa: F401
 from .identity import identity  # noqa: F401
 from .ecrecover import ecrecover  # noqa: F401
 from .ripemd160 import ripemd160  # noqa: F401
+from .modexp import modexp  # noqa: F401
