ee823f9c5818d379ec648a7986a608a0e155c3d5
0xDomCobb <0xdomcobb@proton.me>
2024-05-02 13:28:59 +0800

    update(precompiles): ecpairing

--- xvm/precompiles/__init__.py
+++ xvm/precompiles/__init__.py
@@ -5,3 +5,4 @@ from .ripemd160 import ripemd160  # noqa: F401
 from .modexp import modexp  # noqa: F401
 from .ecadd import ecadd  # noqa: F401
 from .ecmul import ecmul  # noqa: F401
+from .ecpairing import ecpairing  # noqa: F401
